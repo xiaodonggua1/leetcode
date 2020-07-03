f5650a0d43ec98495293546f96fad34e9d33947e
TCL <1371989619@qq.com>
2020-07-03 18:59:12 +0800

    将有序数组转换为二叉搜索树-简单.py

---
+++ 将有序数组转换为二叉搜索树-简单.py
@@ -0,0 +1,39 @@
+"""
+将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定有序数组: [-10,-3,0,5,9],
+
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+"""
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def sortedArrayToBST(nums: List[int]) -> TreeNode:
+    def helper(left, right):
+        if left > right:
+            return None
+        mid = (left + right) // 2
+        root = TreeNode(nums[mid])
+        root.left = helper(left, mid - 1)
+        root.right = helper(mid + 1, right)
+
+        return root
+
+    return helper(0, len(nums) - 1)
+
